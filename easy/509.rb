2b86d2b9fea71aea42c2a2aacc181defb3b3808c
tekitouuser2018 <booty6118@gmail.com>
2021-06-21 15:22:19 +0900

    509 ruby ３項演算子へ変更

--- easy/509.rb
+++ easy/509.rb
@@ -1,12 +1,6 @@
 # @param {Integer} n
 # @return {Integer}
 def fib(n)
-    if n == 0 
-        return 0
-    end
-    if n <= 2
-        return 1
-    end
-    return fib(n-1) + fib(n-2)
+    n == 0 ? (return 0) : n <= 2 ? (return 1) : (return fib(n-1) + fib(n-2))
 end
 
