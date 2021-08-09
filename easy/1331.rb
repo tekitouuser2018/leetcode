d9b748134bf0b3a81704f35d1ee04c506c7129ed
tekitouuser2018 <booty6118@gmail.com>
2021-08-09 16:59:49 +0900

    easy 1331 ruby

---
+++ easy/1331.rb
@@ -0,0 +1,38 @@
+# @param {Integer[]} arr
+# @return {Integer[]}
+def array_rank_transform(arr)
+
+
+    arrayT =[]
+
+    arr.each_with_index do |e, index|
+       arrayT << {num: e, index: index, sorted: 0 } 
+    end
+    p arrayT
+    
+    arr2 = arr.sort
+    arrayT.sort_by! {|a| a[:num]}
+    
+    p arr2
+    p arrayT
+    
+    # arr3 = arr2.clone
+    count = 1
+    arr2.each_with_index do |num, index|
+       if index > 0 && arr2[index] == arr2[index-1]
+            # arr3[index] = arr3[index-1]
+            arrayT[index][:sorted] = arrayT[index-1][:sorted]
+            next
+       else
+           # arr3[index] = count
+           arrayT[index][:sorted] = count
+        end
+        count += 1
+    end
+    p arr3
+    
+    # arr = arr.map{|e| arr3[arr2.index(e)]}.to_a
+
+    return arrayT.sort_by{|a| a[:index]}.map{|a| a[:sorted]}.to_a
+end
+
