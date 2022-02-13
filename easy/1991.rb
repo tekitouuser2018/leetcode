c029ffd49dabe51ece786a645d6842db983373b7
tekitouuser2018 <booty6118@gmail.com>
2022-02-13 14:39:03 +0900

    easy 1991 ruby

---
+++ easy/1991.rb
@@ -0,0 +1,26 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def find_middle_index(nums)
+    len = nums.length
+    nums.each_with_index do |x, i|
+        leftSum = 0
+        rightSum = 0
+        if i < len -1
+            nums[i+1...len].each do |r| 
+                rightSum += r
+            end
+        end
+        if i > 0
+            (0...i).each{ |j| 
+                if j < i
+                    leftSum += nums[j]
+                end
+            }
+        end
+        
+        if leftSum == rightSum
+           return i 
+        end
+    end
+    return -1
+end
